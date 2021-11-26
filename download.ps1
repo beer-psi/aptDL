241e3c54b725d06ad4ecbd0b90c636d734cdfb36
extradummythicc <lacvtg.a1.2023@gmail.com>
2021-11-26 12:39:29 +0700

    Major rewrite

---
+++ download.ps1
@@ -0,0 +1,209 @@
+function Get-UserAgent() {
+    return "Telesphoreo APT-HTTP/1.0.999"
+}
+
+function strip_filename($path) { $path -replace [regex]::escape((fname $path)) }
+function fname($path) { split-path $path -leaf }
+
+function filesize($length) {
+    $gb = [math]::pow(2, 30)
+    $mb = [math]::pow(2, 20)
+    $kb = [math]::pow(2, 10)
+
+    if($length -gt $gb) {
+        "{0:n1} GB" -f ($length / $gb)
+    } elseif($length -gt $mb) {
+        "{0:n1} MB" -f ($length / $mb)
+    } elseif($length -gt $kb) {
+        "{0:n1} KB" -f ($length / $kb)
+    } else {
+        "$($length) B"
+    }
+}
+
+function url_remote_filename($url) {
+    $uri = (New-Object URI $url)
+    $basename = Split-Path $uri.PathAndQuery -Leaf
+    If ($basename -match ".*[?=]+([\w._-]+)") {
+        $basename = $matches[1]
+    }
+    If (($basename -notlike "*.*") -or ($basename -match "^[v.\d]+$")) {
+        $basename = Split-Path $uri.AbsolutePath -Leaf
+    }
+    If (($basename -notlike "*.*") -and ($uri.Fragment -ne "")) {
+        $basename = $uri.Fragment.Trim('/', '#')
+    }
+    return $basename
+}
+
+function Get-Header {
+    $headers = @{
+        "X-Machine" = "iPhone10,5"
+        "X-Unique-ID" = "0000000000000000000000000000000000000000"
+        "X-Firmware" = "14.8"
+        "User-Agent" = "Telesphoreo APT-HTTP/1.0.999"
+    }
+    return $headers
+}
+
+function dl($url, $to, $cookies, $progress, $prepend = "") {
+    $reqUrl = ($url -split "#")[0]
+    $wreq = [net.webrequest]::create($reqUrl)
+    if($wreq -is [net.httpwebrequest]) {
+        $wreq.useragent = Get-UserAgent
+        $wreq.headers.add('X-Machine', 'iPhone10,5')
+        $wreq.headers.add('X-Unique-ID', '0000000000000000000000000000000000000000')
+        $wreq.headers.add('X-Firmware', '14.8')
+        if (-not ($url -imatch "sourceforge\.net" -or $url -imatch "portableapps\.com")) {
+            $wreq.referer = strip_filename $url
+        }
+        if($cookies) {
+            $wreq.headers.add('Cookie', (cookie_header $cookies))
+        }
+    }
+
+    try {
+        $wres = $wreq.GetResponse()
+    } catch [System.Net.WebException] {
+        $exc = $_.Exception
+        $handledCodes = @(
+            [System.Net.HttpStatusCode]::MovedPermanently,  # HTTP 301
+            [System.Net.HttpStatusCode]::Found,             # HTTP 302
+            [System.Net.HttpStatusCode]::SeeOther,          # HTTP 303
+            [System.Net.HttpStatusCode]::TemporaryRedirect  # HTTP 307
+        )
+
+        # Only handle redirection codes
+        $redirectRes = $exc.Response
+        if ($handledCodes -notcontains $redirectRes.StatusCode) {
+            throw $exc
+        }
+
+        # Get the new location of the file
+        if ((-not $redirectRes.Headers) -or ($redirectRes.Headers -notcontains 'Location')) {
+            throw $exc
+        }
+
+        $newUrl = $redirectRes.Headers['Location']
+        info "Following redirect to $newUrl..."
+
+        # Handle manual file rename
+        if ($url -like '*#/*') {
+            $null, $postfix = $url -split '#/'
+            $newUrl = "$newUrl#/$postfix"
+        }
+
+        dl $newUrl $to $cookies $progress
+        return
+    }
+
+    $total = $wres.ContentLength
+    if($total -eq -1 -and $wreq -is [net.ftpwebrequest]) {
+        $total = ftp_file_size($url)
+    }
+
+    if ($progress -and ($total -gt 0)) {
+        [console]::CursorVisible = $false
+        function dl_onProgress($read) {
+            dl_progress $read $total ([System.IO.Path]::GetFileName($to)) $prepend
+        }
+    } else {
+        $temp = ([System.IO.Path]::GetFileName($to))
+        write-host "Downloading $temp ($(filesize $total))..."
+        function dl_onProgress {
+            #no op
+        }
+    }
+
+    try {
+        $s = $wres.getresponsestream()
+        $fs = [io.file]::openwrite($to)
+        $buffer = new-object byte[] 2048
+        $totalRead = 0
+        $sw = [diagnostics.stopwatch]::StartNew()
+
+        dl_onProgress $totalRead
+        while(($read = $s.read($buffer, 0, $buffer.length)) -gt 0) {
+            $fs.write($buffer, 0, $read)
+            $totalRead += $read
+            if ($sw.elapsedmilliseconds -gt 100) {
+                $sw.restart()
+                dl_onProgress $totalRead
+            }
+        }
+        $sw.stop()
+        dl_onProgress $totalRead
+    } finally {
+        if ($progress) {
+            [console]::CursorVisible = $true
+            write-host
+        }
+        if ($fs) {
+            $fs.close()
+        }
+        if ($s) {
+            $s.close();
+        }
+        $wres.close()
+    }
+}
+
+function dl_progress($read, $total, $url, $prepend) {
+    $console = $host.UI.RawUI;
+    $left  = $console.CursorPosition.X;
+    $top   = $console.CursorPosition.Y;
+    $width = $console.BufferSize.Width;
+
+    if($read -eq 0) {
+        $maxOutputLength = $(dl_progress_output $url 100 $total $console).length
+        if (($left + $maxOutputLength) -gt $width) {
+            # not enough room to print progress on this line
+            # print on new line
+            write-host
+            $left = 0
+            $top  = $top + 1
+            if($top -gt $console.CursorPosition.Y) { $top = $console.CursorPosition.Y }
+        }
+    }
+    write-host "`r" -nonewline
+    write-host $(dl_progress_output $url $read $total $console $prepend) -nonewline
+}
+
+function dl_progress_output($url, $read, $total, $console, $prepend) {
+    $filename = $url
+
+    # calculate current percentage done
+    $p = [math]::Round($read / $total * 100, 0)
+
+    # pre-generate LHS and RHS of progress string
+    # so we know how much space we have
+    $left  = "$prepend $filename ($(filesize $total))"
+    $right = [string]::Format("{0,3}%", $p)
+
+    # calculate remaining width for progress bar
+    $midwidth  = $console.BufferSize.Width - ($left.Length + $right.Length + 8)
+
+    # calculate how many characters are completed
+    $completed = [math]::Abs([math]::Round(($p / 100) * $midwidth, 0) - 1)
+
+    # generate dashes to symbolise completed
+    if ($completed -gt 1) {
+        $dashes = [string]::Join("", ((1..$completed) | ForEach-Object {"="}))
+    }
+
+    # this is why we calculate $completed - 1 above
+    $dashes += switch($p) {
+        100 {"="}
+        default {">"}
+    }
+
+    # the remaining characters are filled with spaces
+    $spaces = switch($dashes.Length) {
+        $midwidth {[string]::Empty}
+        default {
+            [string]::Join("", ((1..($midwidth - $dashes.Length)) | ForEach-Object {" "}))
+        }
+    }
+
+    "$left [$dashes$spaces] $right"
+}
--- main.ps1
+++ main.ps1
@@ -1,71 +1,43 @@
 param (
-    [string][Parameter(Mandatory=$true)][alias('s')]$link,
-    [switch][alias('orig','keep')]$original,
-    [switch][alias('format')]$formatted,
-    [string][alias('o')]$output = ".\output",
+    [string][Parameter(Position=0)][alias('s')]$link,
+    [string][alias('f')]$file,
+    [switch][alias('orig','keep','co')]$original,
+    [switch][alias('format','cr')]$formatted,
+    [string][Parameter(Position=1)][alias('o')]$output = ".\output",
     [double][alias('c','cd')]$cooldown = 5
 )
-if (-not ($original_filenames -or $formatted_filenames)){
-    $formatted_filenames = $true
-}
+$formatted = $true
 
-
+. "$PSScriptRoot/download.ps1"
 $files = @(".\7za.exe", ".\7za.dll", ".\7zxa.dll")
 foreach ($file in $files){
     if (-not (Test-Path $file)){
         throw "Could not find required 7zip files. Download from https://www.7-zip.org/a/7z1900-extra.7z and put them in the script's directory"
     }
 }
-if (-not (Test-Path $output)) {
-    mkdir $output
-}
 
-function Format-Urls{
-    param (
-        [string][Parameter(Mandatory=$true)]$url
-    )
-    #if (!$url.StartsWith("http://") || !$url.StartsWith("https://")){
-    #    $url = 'https://' + $url
-    #}
-    if (!$url.EndsWith("/")){
-        $url = $url + "/"
+function Get-Repo($url, $output, $cooldown, $keep = $true) {
+    $output = Join-Path $PSScriptRoot $output
+    if (-not (Test-Path $output)) {
+        mkdir $output
     }
-    return $url
-}
+    $url = Format-Url -url $url
 
-function Get-Headers {
-    headers = @{
-        "X-Machine" = "iPod4,1"
-        "X-Unique-ID" = "0000000000000000000000000000000000000000"
-        "X-Firmware" = "6.1"
-        "User-Agent" = "Telesphoreo APT-HTTP/1.0.999"
-    }
-    return $headers
-}
-
-Function Get-Repo {
-    param (
-        [string][Parameter(Mandatory=$true)]$url,
-        [switch]$keep,
-        [string]$output,
-        [string]$cooldown
-    )
-    $url = Format-Urls -url $url
     $exts = @(".bz2", "", ".xz", ".gz", ".lzma")
     $compressed = @{
         status = $false
         format = ""
     }
+    $old = $ProgressPreference
+    $ProgressPreference = "SilentlyContinue"
     foreach ($ext in $exts){
-        Write-Output "Attempting to download Packages$ext"
-        ($url + "Packages" + $ext)
-        $package = Invoke-WebRequest ($url + "Packages" + $ext)
+        Write-Color "==> Attempting to download Packages$ext" -color Blue
+        $package = Invoke-WebRequest ($url + "Packages" + $ext) -Headers (Get-Header) -Method Head
         if ($package.StatusCode -ne 200) {
-            Write-Output "Couldn't download Packages$ext"
+            Write-Color " -> Couldn't download Packages$ext" -color Red
             continue
         } else {
-            Out-File "Packages$ext"
-            Invoke-WebRequest ($url + "Packages" + $ext) -OutFile ("Packages" + $ext)
+            Invoke-WebRequest ($url + "Packages" + $ext) -OutFile ("Packages" + $ext) -Headers (Get-Header)
             if ($ext -ne "") {
                 $compressed.status = $true
                 $compressed.format = $ext
@@ -73,9 +45,11 @@ Function Get-Repo {
             break
         }
     }
+    $ProgressPreference = $old
     if ($compressed.status){
         .\7za.exe e ("Packages" + $compressed.format) -aoa
     }
+
     $linksList = @()
     $namesList = @()
     $versList = @()
@@ -85,25 +59,64 @@ Function Get-Repo {
         if ($_.StartsWith("Filename: ")) {$linksList += $_ -replace '^Filename: '}
     }
     $length = $linksList.length
-    Clear-Host
-    for ($i = 1; $i -le $linksList.length; $i++) {
-        $percentage = $i / $length * 100
+    for ($i = 0; $i -lt $length; $i++) {
+        $curr = $i + 1
+        $prepend = "($curr/$length)"
         if (!$keep) {
             $filename = $namesList[$i] + "-" + $versList[$i] + ".deb"
-            Write-Host "($i/$length) Downloading $filename" -ForegroundColor DarkCyan
-            aria2c --continue --no-conf --async-dns=false --console-log-level=warn -j1 -x1 -s1 -o (Join-Path $output $filename) ($url + $linksList[$i]) 
         }
         else {
-            Write-Output "($i/$length) Downloading $linksList[$i]"
-            aria2c --continue --no-conf --async-dns=false --console-log-level=warn -j1 -x1 -s1 --dir $output ($url + $linksList[$i]) 
+            $filename = [System.IO.Path]::GetFileName($linksList[$i])
         }
+
+        if (!(Test-Path (Join-Path $output $namesList[$i]))) {
+            mkdir (Join-Path $output $namesList[$i]) > $null
+        }
+
+        try {
+            dl ($url + $linksList[$i]) (Join-Path $output $namesList[$i] $filename) "" $true $prepend
+        }
+        catch {
+            try { 
+                $exc = (($Error[0].Exception | Select-String ":(?:.*):(.*)").Matches.groups[1].value | Select-String "^(.*?)[.?!]\s").Matches.groups[0].value
+            }
+            catch {
+                $exc = $Error[0].Exception
+            }
+            Write-Color "$prepend Download for $filename failed:$exc" -color Red
+        } 
         Start-Sleep -Seconds ([double]$cooldown)
     }
 }
 
-if ($original_filenames){
-    Get-Repo -url $link -keep -output $output -cooldown $cooldown
-} else {
-    Get-Repo -url $link -output $output -cooldown $cooldown
+function Write-Color {
+    param (
+        [string][Parameter(Mandatory=$true, Position=0)]$str,
+        [string][Parameter(Mandatory=$true, Position=1)]$color
+    )
+    $t = $host.ui.RawUI.ForegroundColor
+    if ($color -in [enum]::GetValues([System.ConsoleColor])) {
+        $host.ui.RawUI.ForegroundColor = $color
+    }
+    else {
+        throw 'Invalid color.'
+    }
+    Write-Output $str
+    $host.ui.RawUI.ForegroundColor = $t
 }
+
+function Format-Url {
+    param (
+        [string][Parameter(Mandatory=$true)]$url
+    )
+    if (!($url.StartsWith("http://") -or $url.StartsWith("https://"))){
+        $url = 'https://' + $url
+    }
+    if (!$url.EndsWith("/")){
+        $url = $url + "/"
+    }
+    return $url
+}
+
+Get-Repo $link $output $cooldown $formatted
 
