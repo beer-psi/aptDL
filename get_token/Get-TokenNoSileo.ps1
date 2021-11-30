0871051b8fe53b68717cb37b26f84844eb7aab19
extradummythicc <lacvtg.a1.2023@gmail.com>
2021-11-30 11:38:37 +0700

    Preliminary method to get tokens without needing browser

---
+++ get_token/Get-TokenNoSileo.ps1
@@ -0,0 +1,82 @@
+<#
+.SYNOPSIS
+    A helper script to get the authentication token for commercial repos.
+.PARAMETER url
+    The url of the repo to be authenticated with
+.PARAMETER output
+    Where to write the complete authentication.json
+.PARAMETER udid
+    Your device's UDID
+.PARAMETER model
+    Your device's identifier (e.g. iPhone10,5)
+.PARAMETER cookies
+    Location of dumped cookies.txt
+.PARAMETER curl
+    Location of curl
+.PARAMETER grep
+    Location of grep
+.NOTES
+    Author: beerpsi/extradummythicc
+    Portions of the code was taken from Scoop (https://github.com/ScoopInstaller/Scoop/)
+#>
+param (
+    [Parameter(Position=0, Mandatory,
+        HelpMessage="Repo to authenticate with")]
+    [string]$url,
+
+    [Parameter(Position=1, Mandatory,
+        HelpMessage="UDID of the device")]
+    [string]$udid,
+
+    [Parameter(Position=2, Mandatory,
+        HelpMessage="Device identifier (e.g: iPhone10,5)")]
+    [string]$model,
+
+    [Parameter(Position=3, Mandatory,
+         HelpMessage="Location of dumped cookies.txt")]
+    [string]$cookies,
+
+    [Parameter(Position=4,
+        HelpMessage="Where to write thze authentication file")]
+    [string]$output = "authentication.json",
+
+    [string]$curl = (Get-Command curl).Source,
+
+    [string]$grep = (Get-Command grep).Source
+)
+. "$PSScriptRoot\..\modules\download.ps1"
+. "$PSScriptRoot\..\modules\helper.ps1"
+. "$PSScriptRoot\..\modules\repo.ps1"
+$url = Format-Url $url
+$endpoint = (Get-PaymentEndpoint $url).TrimEnd('/')
+$cookies = Resolve-Path $cookies -ErrorAction Stop
+$callback = ""
+
+$weird_repos = @("https://repo.dynastic.co/")
+if ($weird_repos -contains $url){
+    throw "This repo needs further action. Refer to the wiki:`nhttps://github.com/extradummythicc/aptDL/wiki/Custom-workarounds-to-get-the-token-if-you-cannot-register-the-Sileo-URL-protocol"
+}
+
+switch ($url) {
+    "https://repo.chariz.com/" {
+        $callback = curl -X POST -b $cookies -d "udid=$udid" -d "model=$model" "https://chariz.com/api/sileo/authenticate?udid=$udid&model=$model"
+        break
+    } 
+    default {
+        $callback = & $curl -v -A (Get-UserAgent) -b $cookies "$endpoint/authenticate?udid=$udid&model=$model" 2>&1
+        break
+    }
+}
+$callback = $callback | grep -Eo -m 1 "sileo:\/\/[a-zA-Z0-9.\/?=_%:|&;-]*"
+$sileoqs = Resolve-SileoQueryString $callback
+
+$authentication = @{
+    udid = $udid
+    device = $model
+    token = $sileoqs.token[0]
+}
+ConvertTo-Json $authentication | Out-File -Encoding UTF8 (Resolve-PathForced $output)
+
+
+
+
