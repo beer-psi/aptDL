94c9055febd670884940834951e83a8c621de288
extradummythicc <lacvtg.a1.2023@gmail.com>
2021-12-10 22:01:24 +0700

    code formatting

--- modules/helper.ps1
+++ modules/helper.ps1
@@ -1,10 +1,10 @@
 function Rename-InvalidFileNameChar {
     param(
-        [Parameter(Mandatory=$true,Position=0)]
+        [Parameter(Mandatory = $true, Position = 0)]
         [String[]]$Name,
 
-        [Parameter(Position=1)]
-        [String]$Replacement=''
+        [Parameter(Position = 1)]
+        [String]$Replacement = ''
     )
     $arrInvalidChars = [System.IO.Path]::GetInvalidFileNameChars()
     $invalidChars = [RegEx]::Escape(-join $arrInvalidChars)
@@ -14,12 +14,12 @@ function Rename-InvalidFileNameChar {
 
 function Resolve-PathForced {
     param (
-        [Parameter(Position=0, Mandatory)]
+        [Parameter(Position = 0, Mandatory)]
         [string]$FileName
     )
 
     $FileName = Resolve-Path $FileName -ErrorAction SilentlyContinue `
-                                       -ErrorVariable _frperror
+        -ErrorVariable _frperror
     if (-not($FileName)) {
         $FileName = $_frperror[0].TargetObject
     }
@@ -29,28 +29,28 @@ function Resolve-PathForced {
 
 function Resolve-SileoQueryString {
     param (
-        [Parameter(Position=0, Mandatory)]
+        [Parameter(Position = 0, Mandatory)]
         [string]$url
     )
-    $iqs = $url.IndexOf("?")
+    $iqs = $url.IndexOf('?')
     if ($iqs -lt $url.Length - 1) {
         $querystring = $url.Substring($iqs + 1)
     }
     else {
-        $querystring = ""
+        $querystring = ''
     }
     $query = [System.Web.HttpUtility]::ParseQueryString($querystring)
     $output = @{
-        token = $query.GetValues("token")
-        payment_secret = $query.GetValues("payment_secret")
+        token          = $query.GetValues('token')
+        payment_secret = $query.GetValues('payment_secret')
     }
     return $output
 }
 
 function Write-Color {
     param (
-        [string][Parameter(Mandatory=$true, Position=0)]$str,
-        [string][Parameter(Mandatory=$true, Position=1)]$color
+        [string][Parameter(Mandatory = $true, Position = 0)]$str,
+        [string][Parameter(Mandatory = $true, Position = 1)]$color
     )
     $t = $host.ui.RawUI.ForegroundColor
     if ($color -in [enum]::GetValues([System.ConsoleColor])) {
@@ -65,24 +65,24 @@ function Write-Color {
 
 function Format-Url {
     param (
-        [string][Parameter(Mandatory=$true, Position=0)]$url
+        [string][Parameter(Mandatory = $true, Position = 0)]$url
     )
-    if (!($url.StartsWith("http://") -or $url.StartsWith("https://"))){
+    if (!($url.StartsWith('http://') -or $url.StartsWith('https://'))) {
         $url = 'https://' + $url
     }
-    if (!$url.EndsWith("/")){
-        $url = $url + "/"
+    if (!$url.EndsWith('/')) {
+        $url = $url + '/'
     }
     return $url
 }
 
 function Format-InputData {
     param (
-        [Parameter(Mandatory=$true, Position=0)]
+        [Parameter(Mandatory = $true, Position = 0)]
         [Hashtable]$inputfile
     )
     $outputs = $inputfile
-    $globalValues = @("cooldown", "original", "output", "skipDownloaded")
+    $globalValues = @('cooldown', 'original', 'output', 'skipDownloaded')
     foreach ($globalValue in $globalValues) {
         if ($outputs.ContainsKey($globalValue)) {
             foreach ($output in $outputs.All) {
@@ -102,19 +102,19 @@ function Get-7zExec {
     $7z = (Get-Command 7z -ErrorAction SilentlyContinue).Source
     if ($null -eq $7z) {
         switch ($PSVersionTable.Platform) {
-            "Win32NT" {
+            'Win32NT' {
                 $files = @("$PSScriptRoot\..\7za.exe", "$PSScriptRoot\..\7za.dll", "$PSScriptRoot\..\7zxa.dll")
-                foreach ($file in $files){
-                    if (-not (Test-Path $file)){
+                foreach ($file in $files) {
+                    if (-not (Test-Path $file)) {
                         throw "Could not find required 7zip files. Download from https://www.7-zip.org/a/7z1900-extra.7z and put them in the script's directory`nIf you already have 7zip installed, add it to your PATH."
                         exit
                     }
                 }
                 $7z = (Resolve-Path "$PSScriptRoot\..\7za.exe")
             }
-            "Unix" {
-               throw "7z is either not installed, or not available in PATH. Install it from your package manager, or add it to your PATH."
-               exit
+            'Unix' {
+                throw '7z is either not installed, or not available in PATH. Install it from your package manager, or add it to your PATH.'
+                exit
             }
         }
     }
