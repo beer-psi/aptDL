5c015984256a5a48938d20a2daa677f150c36e26
extradummythicc <lacvtg.a1.2023@gmail.com>
2021-12-11 07:10:46 +0700

    Fix for $PSVersionTable.Platform not available on $PSEdition: Desktop

--- modules/helper.ps1
+++ modules/helper.ps1
@@ -101,7 +101,8 @@ function Format-InputData {
 function Get-7zExec {
     $7z = (Get-Command 7z -ErrorAction SilentlyContinue).Source
     if ($null -eq $7z) {
-        switch ($PSVersionTable.Platform) {
+        $PSPlatform = ($PSVersionTable.Platform, "Win32NT")[$PSEdition -eq "Desktop"]
+        switch ($PSPlatform) {
             'Win32NT' {
                 $files = @("$PSScriptRoot\..\7za.exe", "$PSScriptRoot\..\7za.dll", "$PSScriptRoot\..\7zxa.dll")
                 foreach ($file in $files) {
